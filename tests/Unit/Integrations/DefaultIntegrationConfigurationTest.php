9f073c78b14df70f4615a7960cc3339fa42e7653
Sammy Powers <sammyk@datadoghq.com>
2020-07-20 16:45:40 -0700

    Support DD_TRACE_<INTEGRATION>_<SUFFIX> env vars

--- tests/Unit/Integrations/DefaultIntegrationConfigurationTest.php
+++ tests/Unit/Integrations/DefaultIntegrationConfigurationTest.php
@@ -11,68 +11,68 @@ final class DefaultIntegrationConfigurationTest extends BaseTestCase
     {
         parent::setUp();
         putenv('DD_TRACE_ANALYTICS_ENABLED');
-        putenv('DD_DUMMY_ANALYTICS_ENABLED');
-        putenv('DD_DUMMY_ANALYTICS_SAMPLE_RATE');
+        putenv('DD_PDO_ANALYTICS_ENABLED');
+        putenv('DD_PDO_ANALYTICS_SAMPLE_RATE');
     }
 
     public function testTraceAnalyticsOffByDefault()
     {
-        $conf = new DefaultIntegrationConfiguration('dummy');
+        $conf = new DefaultIntegrationConfiguration('pdo');
         $this->assertFalse($conf->isTraceAnalyticsEnabled());
     }
 
     public function testTraceAnalyticsIfIntegrationEnabled()
     {
-        putenv('DD_DUMMY_ANALYTICS_ENABLED=true');
-        $conf = new DefaultIntegrationConfiguration('dummy');
+        putenv('DD_PDO_ANALYTICS_ENABLED=true');
+        $conf = new DefaultIntegrationConfiguration('pdo');
         $this->assertTrue($conf->isTraceAnalyticsEnabled());
     }
 
     public function testTraceAnalyticsGlobalEnabledAndNotRequiresExplicit()
     {
         putenv('DD_TRACE_ANALYTICS_ENABLED=true');
-        $conf = new DefaultIntegrationConfiguration('dummy', false);
+        $conf = new DefaultIntegrationConfiguration('pdo', false);
         $this->assertTrue($conf->isTraceAnalyticsEnabled());
     }
 
     public function testTraceAnalyticsGlobalEnabledAndRequiresExplicit()
     {
         putenv('DD_TRACE_ANALYTICS_ENABLED=true');
-        $conf = new DefaultIntegrationConfiguration('dummy');
+        $conf = new DefaultIntegrationConfiguration('pdo');
         $this->assertFalse($conf->isTraceAnalyticsEnabled());
     }
 
     public function testTraceAnalyticsIntegrationEnabledAndRequiresExplicit()
     {
-        putenv('DD_DUMMY_ANALYTICS_ENABLED=true');
-        $conf = new DefaultIntegrationConfiguration('dummy');
+        putenv('DD_PDO_ANALYTICS_ENABLED=true');
+        $conf = new DefaultIntegrationConfiguration('pdo');
         $this->assertTrue($conf->isTraceAnalyticsEnabled());
     }
 
     public function testTraceAnalyticsGlobalDisabledIntegrationEnabledRequiresExplicit()
     {
         putenv('DD_TRACE_ANALYTICS_ENABLED=false');
-        putenv('DD_DUMMY_ANALYTICS_ENABLED=true');
-        $conf = new DefaultIntegrationConfiguration('dummy');
+        putenv('DD_PDO_ANALYTICS_ENABLED=true');
+        $conf = new DefaultIntegrationConfiguration('pdo');
         $this->assertTrue($conf->isTraceAnalyticsEnabled());
     }
 
     public function testTraceAnalyticsSampleRateDefaultTo1()
     {
-        $conf = new DefaultIntegrationConfiguration('dummy');
+        $conf = new DefaultIntegrationConfiguration('pdo');
         $this->assertEquals(1.0, $conf->getTraceAnalyticsSampleRate());
     }
 
     public function testTraceAnalyticsSampleRateCanBeSet()
     {
-        putenv('DD_DUMMY_ANALYTICS_SAMPLE_RATE=0.3');
-        $conf = new DefaultIntegrationConfiguration('dummy');
+        putenv('DD_PDO_ANALYTICS_SAMPLE_RATE=0.3');
+        $conf = new DefaultIntegrationConfiguration('pdo');
         $this->assertEquals(0.3, $conf->getTraceAnalyticsSampleRate());
     }
 
     public function testTraceAnalyticsOffIfGlobalAndIntegrationNotSetAndNotRequiresExplicit()
     {
-        $conf = new DefaultIntegrationConfiguration('dummy', false);
+        $conf = new DefaultIntegrationConfiguration('pdo', false);
         $this->assertFalse($conf->isTraceAnalyticsEnabled());
     }
 }
